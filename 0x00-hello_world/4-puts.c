bbfd7e3b1b1980c53bcc6393e9ed49de8d772384
Emmanuel James <chiderajames55@gmail.com>
2022-10-13 01:19:12 -0700

    print a string using the puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
-* main - print the string in the puts function
-*
-* Description:
-* This program prints "Programming is like building a multilingual puzzle
-*
-* Return: 0 (success)
+ * main - print the string in the puts function
+ *
+ * Description:
+ * This program prints "Programming is like building a multilingual puzzle
+ *
+ * Return: 0 (success)
 */
 int main(void)
 {
