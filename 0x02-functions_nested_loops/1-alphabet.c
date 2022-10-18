bc500dcd4385637b46ea58438e59004ae8767534
Emmanuel James <chiderajames55@gmail.com>
2022-10-18 00:51:55 -0700

    A function passed to the main that prints a-z

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,22 +1,6 @@
 #include "holberton.h"
 
 /**
-  * main - Prints the print_alphabet statements
-  *
-  * Description: This function takes in the print_alphabet
-  * and display all the contents in the function passed
-  * i.e print_alphabet
-  *
-  * Return: 0 (success)
-  */
-void print_alphabet(void);
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
-
-/**
   * print_alphabet - Prints a-z
   *
   * Description: This function prints
