92efc43399e169990f83e54453ee9dc73134d71e
Emmanuel James <chiderajames55@gmail.com>
2022-10-18 00:53:03 -0700

    A function passed to main that prints a-z 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,22 +1,6 @@
 #include "holberton.h"
 
 /**
-  * main - print print_alphabet_x10 statements
-  *
-  * Description: This function prints the
-  * print_alphabet_x10 statements which are
-  * a-z 10 times
-  *
-  * Return: 0 (success)
-  */
-void print_alphabet_x10(void);
-int main(void)
-{
-	print_alphabet_x10();
-	return (0);
-}
-
-/**
   * print_alphabet_x10 - print a-z 10 times
   *
   * Description: This function prints a-z 10 times
