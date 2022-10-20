feee86fb1ecc24efc4747ef205d75cbd7aad27fb
Emmanuel James <chiderajames55@gmail.com>
2022-10-20 06:44:24 -0700

    Prints a diagonal line

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -13,22 +13,17 @@ void print_diagonal(int n)
 {
 	int cunt, diagnl;
 
-	if (n <= 0)
+	for (cunt = 1; cunt <= n; cunt++)
 	{
-		_putchar('\n');
-	}
-	else
-	{
-		for (cunt = 1; cunt <= n; ++cunt)
-		{
-			for (diagnl = 1; diagnl <= i; ++diagnl)
-			{
-				if (diagnl != cunt)
-					_putchar(' ');
-				else
-					_putchar('\\');
-			}
+		if (n <= 0)
 			_putchar('\n');
+		for (diagnl = 1; diagnl <= cunt; diagnl++)
+		{
+			if (diagnl != cunt)
+				_putchar(' ');
+			else
+				_putchar('\\');
 		}
+		_putchar('\n');
 	}
 }
