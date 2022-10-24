844847847acf6c53b71378cd2f1eee0805615098
Emmanuel James <chiderajames55@gmail.com>
2022-10-24 11:43:04 -0700

    Assign a pointer's referenced value to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - update a pointer's referenced value to 98
+ * @n: the number to be updated
+ *
+ * Description: This function updates a pointer referenced value
+ *
+ * Return: void (nothing)
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
