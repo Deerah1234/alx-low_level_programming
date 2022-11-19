43ff80f0f0e615d9abba10ff2c4b577141581189
Emmanuel James <chiderajames55@gmail.com>
2022-11-18 22:11:14 -0800

    add a variable number of integers

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,26 +1,22 @@
-#include <stdarg.h>
 #include "variadic_functions.h"
-
+#include <stdio.h>
+#include <stdarg.h>
 /**
- * sum_them_all - add a variable number of integers
- *
- * @n: number of arguments
- *
- * Return: 0 if n is 0, otherwise sum of the integers
- */
+* sum_them_all - a fun returns the sum of all its parameters
+* @n: number of arguments
+* Return: sum of arguments
+**/
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list nums;
-	int sum;
-	unsigned int i;
+	unsigned int i, sum;
+	va_list args;
 
-	if (n <= 0)
+	if (n == 0)
 		return (0);
-
-	va_start(nums, n);
-	sum = 0;
-	for (i = 0; i < n; ++i)
-		sum += va_arg(nums, int);
-	va_end(nums);
+	va_start(args, n);
+	sum  = 0;
+	for (i = 0; i < n; i++)
+		sum += va_arg(args, int);
+	va_end(args);
 	return (sum);
 }
